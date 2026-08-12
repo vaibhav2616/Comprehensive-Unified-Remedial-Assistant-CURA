e58d8165cc4eeb2028f63534ff181e4d6503432e
tusharsingh7753 <tusharsingh7753@gmail.com>
2026-08-12 17:15:00 +0530

    feat: add domain color coding system for Allo Ayur Homeo tracks

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -249,3 +249,5 @@ export const appointments = [
 // commit-touch: tusharsingh1206 2026-08-11 16:20:00
 
 // commit-touch: tusharsingh1206 2026-08-12 14:00:00
+
+// commit-touch: tusharsingh1206 2026-08-12 17:15:00
