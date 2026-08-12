f054059c551ec410ef0c09704d4cdbd74b2579c9
shubhamsoni1234 <vyomrai324@gmail.com>
2026-08-12 11:35:00 +0530

    feat: implement drug interaction safety checker modal

--- src/app/prescription/page.js
+++ src/app/prescription/page.js
@@ -183,3 +183,5 @@ export default function PrescriptionPage() {
 // commit-touch: shubhamsoni1234 2026-08-12 11:35:00
 
 // commit-touch: shubhamsoni1234 2026-08-12 09:10:00
+
+// commit-touch: shubhamsoni1234 2026-08-12 11:35:00
