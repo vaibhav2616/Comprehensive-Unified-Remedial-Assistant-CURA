b65245158bd4d98c38e30badc7df1cedeefc21d0
shubhamsoni1234 <vyomrai324@gmail.com>
2026-08-13 14:00:00 +0530

    feat: add doctor booking interface with appointment time slots

--- src/app/doctors/page.js
+++ src/app/doctors/page.js
@@ -225,3 +225,5 @@ function DoctorCard({ doctor, onClick }) {
 // commit-touch: shubhamsoni1234 2026-08-13 14:00:00
 
 // commit-touch: shubhamsoni1234 2026-08-13 10:30:00
+
+// commit-touch: shubhamsoni1234 2026-08-13 14:00:00
