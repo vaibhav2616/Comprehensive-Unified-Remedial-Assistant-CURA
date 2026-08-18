c495021f07ab5d3e23f759565ab9475177e7531c
shubhamsoni1234 <vyomrai324@gmail.com>
2026-08-18 12:10:00 +0530

    feat: add prescription approval workflow for doctor review

--- src/app/dashboard/doctor/page.js
+++ src/app/dashboard/doctor/page.js
@@ -188,3 +188,5 @@ function UrgencyBadge({ urgency }) {
 // commit-touch: shubhamsoni1234 2026-08-18 12:10:00
 
 // commit-touch: shubhamsoni1234 2026-08-18 09:30:00
+
+// commit-touch: shubhamsoni1234 2026-08-18 12:10:00
