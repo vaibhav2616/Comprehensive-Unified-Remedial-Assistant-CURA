9bc202ba527b92a0d1c65c84a44631e2bb86ce56
shubhamsoni1234 <vyomrai324@gmail.com>
2026-08-20 13:30:00 +0530

    fix: resolve Navbar import error causing build failure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,3 +89,5 @@ export default function Navbar() {
 // commit-touch: shubhamsoni1234 2026-08-10 16:10:00
 
 // commit-touch: tusharsingh1206 2026-08-18 15:45:00
+
+// commit-touch: shubhamsoni1234 2026-08-20 13:30:00
