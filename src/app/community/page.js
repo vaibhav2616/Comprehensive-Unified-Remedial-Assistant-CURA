0748febef1c32cfc1cca59f148de27d40046f175
shubhamsoni1234 <vyomrai324@gmail.com>
2026-08-30 10:00:00 +0530

    refactor: clean up community page removing all AI buzzword terminology

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -135,3 +135,5 @@ export default function CommunityPage() {
 // commit-touch: shubhamsoni1234 2026-08-30 10:00:00
 
 // commit-touch: shubhamsoni1234 2026-08-16 14:00:00
+
+// commit-touch: shubhamsoni1234 2026-08-30 10:00:00
