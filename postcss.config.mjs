e39c6b377f8dc7003abd35525c80f8214f7e2bdb
shubham1234 <vyomrai324@gmail.com>
2026-09-06 14:00:53 +0530

    chore: sync missing base config files from master

---
+++ postcss.config.mjs
@@ -0,0 +1,15 @@
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default config;
+
+// commit-touch: 2026-08-13 16:45:00
+
+// commit-touch: sameerpatel01 2026-08-13 16:45:00
+
+// commit-touch: sameerpatel01 2026-08-13 16:45:00
+
+// commit-touch: sameerpatel01 2026-08-13 16:45:00
